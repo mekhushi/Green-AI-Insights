b6571c3d32da3d2c396fedb5376ccbd635dc1ad4
Khushi singh <145558409+mekhushi@users.noreply.github.com>
2025-10-24 15:56:07 +0530

    Update experiment.py

--- notebooks/experiment.py
+++ notebooks/experiment.py
@@ -4,6 +4,7 @@ import plotly.express as px
 df = pd.read_csv("../data/emission_results.csv")
 df.head()
 
-# Plot emissions vs accuracy
+
 fig = px.scatter(df, x="emissions_kg", y="accuracy", color="model", size="duration_s")
 fig.show()
+
